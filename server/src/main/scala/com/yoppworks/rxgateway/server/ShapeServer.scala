0b81c26b42dfcd01a9970cad479cba45cce6a904
Martin <martin@byrde.io>
2019-04-26 12:05:23 -0400

    Binding ports for heroku to bind to

--- server/src/main/scala/com/yoppworks/rxgateway/server/ShapeServer.scala
+++ server/src/main/scala/com/yoppworks/rxgateway/server/ShapeServer.scala
@@ -1,17 +1,16 @@
 package com.yoppworks.rxgateway.server
 
 import scala.concurrent.{ExecutionContext, Future}
-
 import akka.actor.ActorSystem
 import akka.http.scaladsl.UseHttp2.Always
 import akka.http.scaladsl.{Http, HttpConnectionContext}
 import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.stream.{ActorMaterializer, Materializer}
+
 import com.typesafe.config.ConfigFactory
 import com.yoppworks.rxgateway.api.ShapeServiceHandler
 
 object ShapeServer {
-  
   def main(args: Array[String]): Unit = {
     val conf = ConfigFactory.parseString("akka.http.server.preview.enable-http2 = on")
       .withFallback(ConfigFactory.defaultApplication())
@@ -22,22 +21,33 @@ object ShapeServer {
 
 class ShapeServer(system: ActorSystem) {
   
-  def run() : Future[ Http.ServerBinding ] = {
+  def run() : Future[Http.ServerBinding] = {
     // Akka boot up code
     implicit val sys : ActorSystem = system
     implicit val mat : Materializer = ActorMaterializer()
     implicit val ec : ExecutionContext = sys.dispatcher
+
+    // Configuration values
+    val config: com.typesafe.config.Config =
+      ConfigFactory.load().resolve()
+
+    val interface: String =
+      config.getString("akka.http.interface")
+
+    val port: Int =
+      config.getInt("akka.http.port")
     
     // Create service handlers
-    val service : HttpRequest => Future[ HttpResponse ] =
+    val service : HttpRequest => Future[HttpResponse] =
       ShapeServiceHandler(ShapeServiceImpl())
     
-    // Bind service handler servers to localhost:8080/8081
-    val binding = Http().bindAndHandleAsync(
-      service,
-      interface = "127.0.0.1",
-      port = 8080,
-      connectionContext = HttpConnectionContext(http2 = Always))
+    // Bind service handler servers to configured values
+    val binding =
+      Http().bindAndHandleAsync(
+        service,
+        interface = interface,
+        port = port,
+        connectionContext = HttpConnectionContext(http2 = Always))
     
     // report successful binding
     binding.foreach {binding =>
