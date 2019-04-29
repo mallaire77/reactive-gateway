08a2b7fd06f9a5d7847350a8783bfa27e542f935
Martin <martin@byrde.io>
2019-04-28 21:15:23 -0400

    Generating shapes on demand and piping them to a stream

--- server/src/main/scala/com/yoppworks/rxgateway/server/ShapeGenerator.scala
+++ server/src/main/scala/com/yoppworks/rxgateway/server/ShapeGenerator.scala
@@ -1,11 +1,50 @@
 package com.yoppworks.rxgateway.server
 
-import com.yoppworks.rxgateway.api.{Shape, TetrisShape}
-class ShapeGenerator {
-  def makeAShape: Shape = {
-    Shape()
-  }
-  def makeATetrisShape: TetrisShape = {
+import com.yoppworks.rxgateway.api.{Color, Opacity, Shape, TetrisShape}
+
+import scala.util.Random
+
+object ShapeGenerator {
+  private val MinNumberOfSides =
+    3
+
+  private val MaxNumberOfSides =
+    10
+
+  private val MinShapeWL =
+    10//px
+
+  private val MaxShapeWL =
+    250//px
+
+  private val MinRotation =
+    0
+
+  private val MaxRotation =
+    360
+
+  def makeAColor: Color =
+    Color(
+      randomWithinRange(0, 255),
+      randomWithinRange(0, 255),
+      randomWithinRange(0, 255))
+
+  def makeAnOpacity: Opacity =
+    Opacity.values(randomWithinRange(0, Opacity.values.length - 1))
+
+  def makeAShape: Shape =
+    Shape(
+      numberOfSides = randomWithinRange(MinNumberOfSides, MaxNumberOfSides),
+      edgeColor = Some(makeAColor),
+      fillColor = Some(makeAColor),
+      opacity = makeAnOpacity,
+      width = randomWithinRange(MinShapeWL, MaxShapeWL),
+      height = randomWithinRange(MinShapeWL, MaxShapeWL),
+      rotation = randomWithinRange(MinRotation, MaxRotation))
+
+  def makeATetrisShape: TetrisShape =
     TetrisShape()
-  }
+
+  private def randomWithinRange(min: Int, max: Int): Int =
+    min + (new Random).nextInt((max - min) + 1)
 }
--- server/src/main/scala/com/yoppworks/rxgateway/server/ShapeServiceImpl.scala
+++ server/src/main/scala/com/yoppworks/rxgateway/server/ShapeServiceImpl.scala
@@ -1,35 +1,48 @@
 package com.yoppworks.rxgateway.server
 
 import scala.concurrent.{ExecutionContext, Future}
-
+import scala.concurrent.duration._
 import akka.NotUsed
-import akka.stream.scaladsl.Source
+import akka.stream.scaladsl.{Flow, Keep, Source}
+
 import com.yoppworks.rxgateway.api._
 
 /** Unit Tests For ShapeServiceImpl */
-case class ShapeServiceImpl()(implicit executor: ExecutionContext) extends
-  ShapeService {
+case class ShapeServiceImpl()(implicit executor: ExecutionContext) extends ShapeService {
   
-  def prepareShapes(in: PrepareShapes): Future[ShapeServiceResult] = Future {
-    ShapeServiceResult()
-  }
+  def prepareShapes(in: PrepareShapes): Future[ShapeServiceResult] =
+    Future {
+      ShapeServiceResult()
+    }
   
-  def getAShape(in: GetAShape): Future[ShapeServiceResult] = Future {
-    ShapeServiceResult()
-  }
+  def getAShape(in: GetAShape): Future[ShapeServiceResult] =
+    Future {
+      ShapeServiceResult()
+    }
   
-  def getSomeShapes(in: GetSomeShapes): Source[Shape, NotUsed] = {
-    Source.empty[Shape]
-  }
+  def getSomeShapes(in: GetSomeShapes): Source[Shape, NotUsed] =
+    Source
+      .tick(0.seconds, in.intervalMs.milliseconds, ShapeGenerator.makeAShape)
+      .zipWithIndex
+      .takeWhile {
+        case (_, idx) =>
+          idx < in.numberOfShapes
+      }
+      .map {
+        case (shape, _) =>
+          shape
+      }
+      .viaMat(Flow[Shape].map(identity))(Keep.right)
   
-  def getSomeTetrisShapes(in: GetSomeTetrisShapes)
-  : Source[TetrisShape, NotUsed] = {
+  def getSomeTetrisShapes(in: GetSomeTetrisShapes): Source[TetrisShape, NotUsed] = {
     Source.empty[TetrisShape]
   }
   
   def releaseShapes(
     in: com.yoppworks.rxgateway.api.ReleaseShapes
-  ): Future[com.yoppworks.rxgateway.api.ShapeServiceResult] = Future {
-    ShapeServiceResult()
-  }
+  ): Future[com.yoppworks.rxgateway.api.ShapeServiceResult] =
+    Future {
+      ShapeServiceResult()
+    }
+
 }
